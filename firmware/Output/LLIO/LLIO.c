ce4a566f72d2b0326e92af154870230683787014
Bruno Freitas <bruno@brunofreitas.com>
2019-10-07 08:27:59 -0300

    Put LL registers and buffers in a clean state if LL is not enabled.

--- firmware/Output/LLIO/LLIO.c
+++ firmware/Output/LLIO/LLIO.c
@@ -56,28 +56,37 @@ void LLIO_Init(void) {
 }
 
 void LLIO_SetPadData(AbstractPad_t *padData) {
+	if (LLIO_LLEnabled()) {
+		//TODO: SNES hardcoded. To change this based on the connected joypad!
+		llio_buffer[0] = 27;
 
-	//TODO: SNES hardcoded. To change this based on the connected joypad!
-	llio_buffer[0] = 27;
+		padData->x ? bit_set(llio_buffer[1], 1 << 0) : bit_clear(llio_buffer[1], 1 << 0); // Y
+		padData->a ? bit_set(llio_buffer[1], 1 << 1) : bit_clear(llio_buffer[1], 1 << 1); // B
+		padData->y ? bit_set(llio_buffer[1], 1 << 2) : bit_clear(llio_buffer[1], 1 << 2); // X
+		padData->b ? bit_set(llio_buffer[1], 1 << 3) : bit_clear(llio_buffer[1], 1 << 3); // A
 
-	padData->x ? bit_set(llio_buffer[1], 1 << 0) : bit_clear(llio_buffer[1], 1 << 0); // Y
-	padData->a ? bit_set(llio_buffer[1], 1 << 1) : bit_clear(llio_buffer[1], 1 << 1); // B
-	padData->y ? bit_set(llio_buffer[1], 1 << 2) : bit_clear(llio_buffer[1], 1 << 2); // X
-	padData->b ? bit_set(llio_buffer[1], 1 << 3) : bit_clear(llio_buffer[1], 1 << 3); // A
+		padData->back  ? bit_set(llio_buffer[1], 1 << 4) : bit_clear(llio_buffer[1], 1 << 4); // Select
+		padData->start ? bit_set(llio_buffer[1], 1 << 5) : bit_clear(llio_buffer[1], 1 << 5); // Start
 
-	padData->back  ? bit_set(llio_buffer[1], 1 << 4) : bit_clear(llio_buffer[1], 1 << 4); // Select
-	padData->start ? bit_set(llio_buffer[1], 1 << 5) : bit_clear(llio_buffer[1], 1 << 5); // Start
+		padData->lb ? bit_set(llio_buffer[1], 1 << 6) : bit_clear(llio_buffer[1], 1 << 6); // L
+		padData->rb ? bit_set(llio_buffer[1], 1 << 7) : bit_clear(llio_buffer[1], 1 << 7); // R
 
-	padData->lb ? bit_set(llio_buffer[1], 1 << 6) : bit_clear(llio_buffer[1], 1 << 6); // L
-	padData->rb ? bit_set(llio_buffer[1], 1 << 7) : bit_clear(llio_buffer[1], 1 << 7); // R
-
-	padData->d_up    ? bit_set(llio_buffer[2], 1 << 2) : bit_clear(llio_buffer[2], 1 << 2); // Up
-	padData->d_down  ? bit_set(llio_buffer[2], 1 << 3) : bit_clear(llio_buffer[2], 1 << 3); // Down
-	padData->d_left  ? bit_set(llio_buffer[2], 1 << 4) : bit_clear(llio_buffer[2], 1 << 4); // Left
-	padData->d_right ? bit_set(llio_buffer[2], 1 << 5) : bit_clear(llio_buffer[2], 1 << 5); // Down
+		padData->d_up    ? bit_set(llio_buffer[2], 1 << 2) : bit_clear(llio_buffer[2], 1 << 2); // Up
+		padData->d_down  ? bit_set(llio_buffer[2], 1 << 3) : bit_clear(llio_buffer[2], 1 << 3); // Down
+		padData->d_left  ? bit_set(llio_buffer[2], 1 << 4) : bit_clear(llio_buffer[2], 1 << 4); // Left
+		padData->d_right ? bit_set(llio_buffer[2], 1 << 5) : bit_clear(llio_buffer[2], 1 << 5); // Down
+	} else {
+		memset(llio_buffer, 0, sizeof(llio_buffer));
+	}
 }
 
 void LLIO_ProcessEvent(void) {
+	
+	// Do nothing if LL is not enabled
+	if (!LLIO_LLEnabled()) {
+		llio_cmd_received = 0;		
+		return;
+	}
 	
 	// Do nothing if latch is pulled low (busy)
 	if (!bit_check(PIND, 0)) {
