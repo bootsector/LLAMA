af5c6ea50d9b2bdddccdc0a053b9f5faec1a6ca9
Bruno Freitas <bruno@brunofreitas.com>
2019-10-08 06:03:15 -0300

    Defining ID bits for supported pads on DB15

--- firmware/Input/Input.h
+++ firmware/Input/Input.h
@@ -23,6 +23,23 @@
 
 #include "AbstractPad.h"
 
+#define DB15_PAD_GENESIS    0b0000
+#define DB15_PAD_RESERVED1  0b0001
+#define DB15_PAD_NES        0b0010
+#define DB15_PAD_SNES       0b0011
+#define DB15_PAD_RESERVED2  0b0100
+#define DB15_PAD_RESERVED3  0b0101
+#define DB15_PAD_RESERVED4  0b0110
+#define DB15_PAD_SATURN     0b0111
+#define DB15_PAD_RESERVED5  0b1000
+#define DB15_PAD_TG16       0b1001
+#define DB15_PAD_RESERVED6  0b1010
+#define DB15_PAD_RESERVED7  0b1011
+#define DB15_PAD_RESERVED8  0b1100
+#define DB15_PAD_RESERVED9  0b1101
+#define DB15_PAD_RESERVED10 0b1110
+#define DB15_PAD_NEOGEO     0b1111
+
 void Input_Init(void);
 void Input_GetPadState(AbstractPad_t *padData);
 
